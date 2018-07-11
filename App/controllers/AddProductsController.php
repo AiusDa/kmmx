66f3a2538da4055729889f6febe46d89454cdba6
Adrián Hidalgo Flores <adrian@MacBook-Pro-de-Adrian.local>
2018-07-10 21:09:09 -0500

    AddProducts and ListProducts implemented

--- App/controllers/AddProductsController.php
+++ App/controllers/AddProductsController.php
@@ -1,7 +1,21 @@
 <?php
     namespace App\controllers;
 
+    use \App\Core\Database;
+
     class AddProductsController extends \App\Core\BaseController
     {
-        
+        public function create($productName, $sku, $price, $qty): bool
+        {
+            try {
+                Database::getInstance()->query("
+                    INSERT INTO products 
+                    (product_name, sku, price, qty) 
+                    VALUES ('$productName', '$sku', '$price', '$qty')
+                ");
+                return true;
+            } catch(Exception $e) {
+                return false;
+            }
+        }
     }
--- App/controllers/ListProductsController.php
+++ App/controllers/ListProductsController.php
@@ -1,7 +1,12 @@
 <?php
     namespace App\controllers;
 
+    use \App\Core\Database;
+
     class ListProductsController extends \App\Core\BaseController
     {
-        
+        public function getProducts()
+        {
+            return Database::getInstance()->query('SELECT * FROM products');
+        }
     }
